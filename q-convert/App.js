f1814f35506b4ccdb72fdde7133bf78055692d8b
Jay VanOrd <jvanord@indasysllc.com>
2018-09-29 14:06:38 -0400

    boilerplate with react-native-responsive-layout

--- q-convert/App.js
+++ q-convert/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Grid, Section, Block} from 'react-native-responsive-layout';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<Grid scrollable stretchable>
+				<Section stretch>
+					<Block size="1/1">
+						<View style={styles.container}>
+							<Text>Empty Boilerplate App</Text>
+						</View>
+					</Block>
+				</Section>
+			</Grid>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
